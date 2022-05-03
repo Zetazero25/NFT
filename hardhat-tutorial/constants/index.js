0c4ae53478db84d4a973eb69e2e8b9c81a7102f4
Justice <justice.williams@spartans.ut.edu>
2022-05-02 20:29:13 -0400

    opensea

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xE5e2B26E6A60244653e27fB34815a9A64916f0BB";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-tan-ten.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
