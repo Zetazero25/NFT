ff81bc274f53f89f8d220d01fa646081851d58f7
Justice <justice.williams@spartans.ut.edu>
2022-05-17 16:08:48 -0400

    new

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract 
 const WHITELIST_CONTRACT_ADDRESS = "0x51cF0dB80eEA54adAb7A7e458254e089E1C52F83";
 // URL to extract Metadata for Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-self.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
